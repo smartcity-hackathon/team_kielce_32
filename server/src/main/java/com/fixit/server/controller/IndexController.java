3445d5f8eaac5113a9432c847824bc82bd12f252
tomaszkowalczyk94 <tomasz.kowalczyk94@gmail.com>
2018-06-15 12:07:53 +0200

    index controller

---
+++ server/src/main/java/com/fixit/server/controller/IndexController.java
@@ -0,0 +1,36 @@
+package com.fixit.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class IndexController {
+
+    class WelcomeMessage {
+
+        public WelcomeMessage(String message) {
+            this.message = message;
+        }
+
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+
+
+    @RequestMapping( value = "/")
+    public WelcomeMessage welcome() throws IOException {
+        return new WelcomeMessage("Welcome to fixit api");
+    }
+
+
+}
